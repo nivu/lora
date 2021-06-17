d1f2edfaa10eed4af9d75718b008eb943d8fce5d
Navaneeth Malingan <navneetnivu07@gmail.com>
2021-06-17 17:56:41 +0530

    lora session and arduino code

--- Python/all.py
+++ Python/all.py
@@ -20,6 +20,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("192.168.1.104", 1883, 60)
+client.connect("192.168.1.102", 1883, 60)
 
 client.loop_forever()
